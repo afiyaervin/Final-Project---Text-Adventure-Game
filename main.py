e52f6ad3a78e67165b305c1b4e64b566d3b9bd38
Oluwadara Dina <oldin1@morgan.edu>
2020-12-09 21:24:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+print("Cinderella's Adventure")
+print("Welcome to  Cinderella's Adventure!")
+print("Type out 'help' if you ever need to access the help menu") 
+user_input = input("There is a ball tonight. Do you want to go?")
+if user == yes:
+  print("You need to get glass slippers, a beautiful dress, and a carriage in time for the Ball tonight! In your inventory you have bread and a wand.")
+else:
+  print("GAME OVER")
+print("The animals have the slippers. You need to find the leader to get the glass slippers.")
+print(" You go to the forest to find the animal leader, and the leader tells you that they are hungry.")
+food_for_animals = input("Would you like to give them any food? (HINT: Check your inventory!)")
+def slippers():
+  slippers_for_cinderella = input("Would you like your glass slippers?")
+  if food_for_animals == yes:
+    if slippers_for_cinderella == yes:
+      print("You gave the Animal Leader bread, so he gave you the glass slippers!")
+    elif:
+      print("TRY AGAIN")  
+slippers()
+print("Now you have to find the dress")
+dress = input("Should you ask your stepmother? Your stepmother may be able to give you one for free!")
+if dress == yes:
+  print("You asked your stepmother for help, but she does not want you to go to the ball.")
+  print("You failed try again.")
+else: 
+
+
+#  If user chooses step mother they fail , else the game continues.
+# On the way to the market Cinderella finds a pumpkin and if she takes the pumpkin it is added to inventory if she does not take the pumpkin she will lose the game.
+#Now the user needs to pick a dress. Three dresses are presented, ballgown, short dress, regular outfit. If she picks the wrong dress she loses the game.
+# print("Alert! Time is running out! You need to get to the Ball!") 
+#print("Hint:Look in your inventory")
+# user input/ print("would you like to select the wand?") if yes, she continues. if no she loses the game 
+#user input -  print('what would you like to use the wand on? The pumpkin, the dress, or the shoes?')
+#if she chooses pumpkin, it turns into a carraige. 
+#if she chooses the dress, it becomes more beautiful 
+#if she chooses the shoes, they become shinier
+#continue asking till she picks the pumpkin 
+#pumpkin turns into a carraige 
+#print('Have fun at the ball!')
+#(Help Menu: Press 'I' for inventory check, press 'b' for go back, )
+#Afiya -if statement, print statements
+#Dara - If statement, print statements
+#Damani - help menu, if statement
+def help():
+  print("[1] Option 1")
+  print("[2] Option 2")
+  print("[0] Exit the program.")
+  
+
+help()
+option =
+
+#what's in the help menu?
+#how many options do we need for the help menu 
+#how to define function with loops?
+#nested if statements?
+#look over it so far  
+#shortcuts??
